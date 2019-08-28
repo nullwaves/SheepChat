4610c90db06467e0805ac8df1bfbdef4f57144dd
evandasheep <jack@auxjack.net>
2019-08-28 03:08:45 -0500

    Create IConnection.cs
    I'm Helping!

---
+++ SheepChat.Server/Interfaces/IConnection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace SheepChat.Server.Interfaces
+{
+    public interface IConnection
+    {
+        /// <summary>
+        /// Gets the ID of the connection.
+        /// </summary>
+        string ID { get; }
+
+        /// <summary>
+        /// Gets or sets the IPAddress for the connection.
+        /// </summary>
+        IPAddress CurrentIPAddress { get; set; }
+
+        /// <summary>
+        /// Gets the byte data waiting to be processed
+        /// </summary>
+        byte[] Data { get; }
+
+        /// <summary>
+        /// Gets the textual representation of the data still waiting to be returned as an input string.
+        /// </summary>
+        StringBuilder Buffer { get; }
+
+        /// <summary>
+        /// Disconnect the connection.
+        /// </summary>
+        void Disconnect();
+
+        /// <summary>
+        /// Send data to the connection.
+        /// </summary>
+        /// <param name="data">The byte array to send.</param>
+        void Send(byte[] data);
+
+        /// <summary>
+        /// Utilize buffer to produce output.
+        /// </summary>
+        void ProcessBuffer();
+    }
+}

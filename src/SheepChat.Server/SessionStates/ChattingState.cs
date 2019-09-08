45db921cb5e60a5787fea590d963686f7539137d
evandasheep <evanb96@gmail.com>
2019-09-08 14:18:13 -0500

    publishing
    stuff

--- src/SheepChat.Server/SessionStates/ChattingState.cs
+++ src/SheepChat.Server/SessionStates/ChattingState.cs
@@ -6,7 +6,7 @@ namespace SheepChat.Server.SessionStates
 {
     internal class ChattingState : SessionState
     {
-        private static List<Session> Who = new List<Session>();
+        private static readonly List<Session> Who = new List<Session>();
 
         public ChattingState(Session session) : base(session)
         {

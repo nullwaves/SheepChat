a83fac204edae0cd067fe458873acd9e66509b1f
evandasheep <evanb96@gmail.com>
2019-09-14 12:40:20 -0500

    Create RoomManager.cs
    Got started, gotta go

---
+++ src/SheepChat.Server/Rooms/RoomManager.cs
@@ -0,0 +1,29 @@
+﻿using SheepChat.Server.Interfaces;
+using System.Collections.Generic;
+
+namespace SheepChat.Server.Rooms
+{
+    public class RoomManager : Manager, IRecomposable
+    {
+        public static RoomManager Instance { get; } = new RoomManager();
+
+        public override string Name => "Rooms";
+
+        public static Dictionary<string, IRoom> RoomList { get; private set; }
+
+        public void Recompose()
+        {
+            Composer.Compose(this);
+        }
+
+        public override void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

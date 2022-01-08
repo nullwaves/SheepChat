b0e6f6472a50859c37b51f67ce8131570a894920
Evan B <evanb96@gmail.com>
2022-01-07 18:27:18 -0600

    SQLite file skeletons

---
+++ src/SheepChat.Server/Data/SQLite/SQLiteRepository.cs
@@ -0,0 +1,57 @@
+﻿using SheepChat.Server.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace SheepChat.Server.Data.SQLite
+{
+    internal class SQLiteRepository<T> : IRepository<T> where T : IModel
+    {
+        internal string _connectionString;
+
+        public SQLiteRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public long Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Filter(Expression<Func<T, bool>> where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SheepChat.Server/Data/SQLite/SQLiteStorageProvider.cs
@@ -0,0 +1,20 @@
+﻿using RepoDb;
+using SheepChat.Server.Data.Interfaces;
+
+namespace SheepChat.Server.Data.SQLite
+{
+    [ExportStorageProvider]
+    internal class SQLiteStorageProvider : IStorageProvider
+    {
+        public string Name => "SQLite";
+
+        public IRepository<T> OpenRepository<T>() where T : IModel
+        {
+            return new SQLiteRepository<T>();
+        }
+
+        public void Prepare()
+        {
+        }
+    }
+}
